7a010875b61b74ada1fe21f1d0b6e59429d246b1
Warren Usui <warren.usui@inktank.com>
2013-09-20 22:18:18 -0700

    Cleanup.  Added passwd directly (for now)

--- teuthology/suite.py
+++ teuthology/suite.py
@@ -440,6 +440,11 @@ def _results(args):
     (subject, body) = build_email_body(args.name, args.archive_dir,
                                        args.timeout)
 
+    for job in get_jobs(args.archive_dir):
+        job_dir = os.path.join(args.archive_dir, job)
+        log.info("DEBUG save in: %s" % job_dir)
+        results_db.store_in_database(job_dir)
+
     try:
         if args.email:
             email_results(
@@ -450,11 +455,6 @@ def _results(args):
             )
     finally:
         generate_coverage(args)
-
-    #for job in get_jobs(args.archive_dir):
-        #job_dir = os.path.join(args.archive_dir, job)
-        #log.info("DEBUG save in: %s" % job_dir)
-        #results_db.store_in_database(job_dir)
 
 
 def get_jobs(archive_dir):
